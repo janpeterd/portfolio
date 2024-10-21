cb484ed4a9a101ed08c28776e2c063546282e263
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2024-10-21 14:17:13 +0200

    generate_pdf_script: prettify + error handling

--- scripts/generate_cv_pdf.js
+++ scripts/generate_cv_pdf.js
@@ -1,35 +1,70 @@
 import puppeteer from 'puppeteer'
 import fs from 'node:fs'
+import { createConnection } from 'node:net'
 
+const hostname = 'localhost'
+const port = 5173
 const pdfPath = './static/doc/cv_Jan-Peter_Dhallé.pdf'
-// const domain = "https://portfolio-one-alpha-86.vercel.app";
-const domain = 'http://localhost:5173'
 const cv_route = '/cv'
-const browser = await puppeteer.launch()
-const page = await browser.newPage()
-await page.goto(domain + cv_route, {
-  waitUntil: 'networkidle2'
-})
+const generatePdfAsync = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(`http://${hostname}:${port}${cv_route}`, {
+    waitUntil: 'networkidle2'
+  })
 
-await page.pdf({
-  path: pdfPath,
-  format: 'a4',
-  margin: {
-    top: '0.5in',
-    bottom: '0.5in',
-    left: '0.5in',
-    right: '0.5in'
+  await page.pdf({
+    path: pdfPath,
+    format: 'a4',
+    margin: {
+      top: '0.5in',
+      bottom: '0.5in',
+      left: '0.5in',
+      right: '0.5in'
+    }
+  })
+
+  await browser.close()
+}
+
+const printPdfFileStats = () => {
+  try {
+    const stats = fs.statSync(pdfPath)
+    console.log(
+      '\x1b[42m SUCCESS: \x1b[0m',
+      `Generated PDF file:\n\tpath:${pdfPath}\n\tsize: ${(stats.size / 1024000).toFixed(2)}MB\n\tcreated: ${stats.ctime}`
+    )
+  } catch (err) {
+    console.error('\x1b[41m ERROR: \x1b[0m', 'Cannot access PDF file for stats.')
+    console.error(err)
   }
-})
+}
 
-await browser.close()
+const checkRunning = () => {
+  return new Promise((resolve) => {
+    const start = performance.now()
+    const socket = createConnection(port, hostname)
+    socket.setTimeout(3000)
+    socket.on('connect', () => {
+      const end = performance.now()
+      socket.end()
+      resolve(end - start)
+    })
+    function handleError() {
+      socket.destroy()
+      resolve(-1)
+    }
+    socket.on('timeout', handleError)
+    socket.on('error', handleError)
+  })
+}
 
-try {
-  const stats = fs.statSync(pdfPath)
-  console.log(
-    `Generated PDF file:\n\tsize: ${(stats.size / 1024000).toFixed(2)}MB\n\tcreated: ${stats.ctime}`
+if ((await checkRunning()) != -1) {
+  await generatePdfAsync()
+  printPdfFileStats()
+} else {
+  console.error(
+    '\x1b[41m ERROR: \x1b[0m',
+    'Cannot connect to server.\nAre you running the dev server? (start with pnpm run dev)'
   )
-} catch (err) {
-  console.error('Cannot access PDF file')
-  console.error(err)
 }
