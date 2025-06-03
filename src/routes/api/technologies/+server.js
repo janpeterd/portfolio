98a0e8c6cbe2636397edcf1f2049163d47e2f3cf
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2025-06-03 17:00:16 +0200

    chore: add JSDoc type annotation for technologyNames

--- src/routes/api/technologies/+server.js
+++ src/routes/api/technologies/+server.js
@@ -2,7 +2,6 @@ import { getTechnologies } from '$lib/technologies'
 import { json } from '@sveltejs/kit'
 
 export async function POST({ request, cookies }) {
-  // this technologyNames is a string[] can you type it with jsdoc AI!
   /** @type {{ technologyNames: string[] }} */
   const { technologyNames } = await request.json()
   const technologies = await getTechnologies()
